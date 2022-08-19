c5c0314feb8953ff5b5af228b2dd079b1009ea35
Lewis Nguyen <lewis.nguyen@terralogic.com>
2022-08-19 15:24:30 +0700

    update link

--- utils/socket.js
+++ utils/socket.js
@@ -1,6 +1,6 @@
 import {io} from 'socket.io-client'
 
-// const socket = io('https://chatchit-server.herokuapp.com');
-const socket = io('http://localhost:1405');
+const socket = io('https://chatchit-server.herokuapp.com');
+// const socket = io('http://localhost:1405');
 
 export default socket
